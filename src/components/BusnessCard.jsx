5ad680a7cd03d9e94e613232fec560d49834c854
Olivia Persson <olivia.persson@forefront.se>
2022-12-21 16:14:44 +0100

    Add basic buisness card

--- src/components/BusnessCard.jsx
+++ src/components/BusnessCard.jsx
@@ -1,7 +1,23 @@
+import profileImage from "../images/temporary-profile-image.png";
 import "./BuisnessCard.css";
 
 function BuisnessCard() {
-  return <h1>My buisness card</h1>;
+  return (
+    <div className="buisness-card">
+      <img
+        className="buisness-card-image"
+        src={profileImage}
+        alt="Profile image"
+      />
+      <div className="card-body">
+        <h2>Olivia Persson</h2>
+        <h5>Software Developer</h5>
+        <p>
+          <small>olivia.persson.website</small>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default BuisnessCard;
